6933a39f37d8d448293775531e6591b652e452d8
xavier blanc <blancxav@gmail.com>
2014-12-27 17:21:52 +0100

    scrollTo + structure fabrice

--- app/js/app.js
+++ app/js/app.js
@@ -24,6 +24,21 @@ app.config(['$routeProvider', function($rp){
 
 app.controller('clubController', ['$scope', '$window', function($scope, $window){
     $('body').scrollspy({ target: '#affix-nav', offset:70})
+
+    $scope.scrollInto = function (elementId) {
+		console.log("scollInto:"+elementId)
+		if(!elementId) $window.scrollTo(0, 0);
+		//check if an element can be found with id attribute
+        
+        var el = document.getElementById(elementId);
+        if(el) {
+            console.log(el.offsetTop);
+            //$window.scrollTo(0,el.offsetTop-60);//scrollIntoView();
+            //$(el).parent().addClass("active");
+            //$(el).parent().removeClass("hover");
+            $($window).scrollTo($(el),800);//scrollIntoView();
+        }
+    }
 }])
 
 
