ef4af24b7d2c965cb12fdb9be652d98f26054442
xavier blanc <blancxav@gmail.com>
2014-12-27 10:03:40 +0100

    structure reste offset à calculer

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,8 @@
-var app=angular.module('appBMX',['ngRoute' , 'ngAnimate', 'waypoint-module'])
+var app=angular.module('appBMX',['ngRoute' , 'ngAnimate'])//, 'waypoint-module'])
 
-app.controller('scrollSpyCtrl', function ($scope, $anchorScroll) {
+/*app.controller('scrollSpyCtrl', function ($scope, $anchorScroll) {
 	$scope.test="ok"
-})
+})*/
 
 app.config(['$routeProvider', function($rp){
 	$rp.
@@ -10,67 +10,28 @@ app.config(['$routeProvider', function($rp){
 			templateUrl: 'views/club.html',
 			controller: 'clubController'
 		}).
-		when('/entrainement', {
-			templateUrl: 'views/entrainement.html'
+		when('/animation', {
+			templateUrl: 'views/animation.html'
 		}).
 		when('/compet', {
-			templateUrl: 'views/compet.html',
-			controller: 'clubController'
+			templateUrl: 'views/compet.html'
 		}).
 		otherwise({
-			templateUrl:'views/home.html'
+			templateUrl:'views/home.html',
+            controller: 'homeController'
 		})
 }])
 
 app.controller('clubController', ['$scope', '$window', function($scope, $window){
-	//prevent default for a of affix
-	$('#navPills a').click(function(event) {event.preventDefault()})
-	$('#navPills a').bind('click', function(event) {scrollInto($(this).attr('href').substring(1))})
-	setSections()
-
-	//click -> scrollTo
-
-	function scrollInto(elementId) {
-		console.log("scollInto:"+elementId)
-		if(!elementId) $window.scrollTo(0, 0);
-		//check if an element can be found with id attribute
-        
-        var el = document.getElementById(elementId);
-        if(el) {
-            console.log(el.offsetTop);
-            $window.scrollTo(0,el.offsetTop-10);//scrollIntoView();
-        }
-    }
-
-    function setSections() {
-    	$('.scrolled-section').each(function() {
-    		$(this).waypoint(function() {
-    			var sectionId = $(this).attr("id");
-
-                // Remove 'current' class from all items
-                $('#navPills li').removeClass('active');
-
-                // Add 'current' class to current item
-                $('#navPills li a[href="#' + sectionId + '"]').parent().addClass("active");
-
-            }, {
-                    offset: 130
-            });
-        });
-
-        $(window).on('scroll', function() {
-        	var windowPos = $(window).scrollTop(); // get the offset of the window from the top of page
-            var windowHeight = $(window).height(); // get the height of the window
-            var docHeight = $(document).height();
-            // If we have reach the bottom of the page
-            if (windowPos + windowHeight === docHeight) {
-                // Remove 'current' class from all items
-                $('#navPills li').removeClass('active');
-                // add current to the last element
-                $('#navPills li').last().addClass("active");
-            }
-        });
-    }
+    $('body').scrollspy({ target: '#affix-nav'})
 }])
 
+
+app.controller('homeController', ['$scope' , '$window', function($scope, $window) {
+    $(function(){
+        $('.carousel').carousel({
+            interval: 2000
+        });
+    });
+}])
 
