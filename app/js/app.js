f166c01ab7f86a9420c741901f3bfa7add5f3794
xavier blanc <blancxav@gmail.com>
2014-12-27 17:53:26 +0100

    structure fab ok

--- app/js/app.js
+++ app/js/app.js
@@ -11,10 +11,12 @@ app.config(['$routeProvider', function($rp){
 			controller: 'clubController'
 		}).
 		when('/animation', {
-			templateUrl: 'views/animation.html'
+			templateUrl: 'views/animation.html',
+			controller: 'animationController'
 		}).
 		when('/compet', {
-			templateUrl: 'views/compet.html'
+			templateUrl: 'views/compet.html',
+			controller: 'competController'
 		}).
 		otherwise({
 			templateUrl:'views/home.html',
@@ -36,7 +38,45 @@ app.controller('clubController', ['$scope', '$window', function($scope, $window)
             //$window.scrollTo(0,el.offsetTop-60);//scrollIntoView();
             //$(el).parent().addClass("active");
             //$(el).parent().removeClass("hover");
-            $($window).scrollTo($(el),800);//scrollIntoView();
+            $($window).scrollTo($(el),1000,  {offset:-60} );//scrollIntoView();
+        }
+    }
+}])
+
+app.controller('animationController', ['$scope', '$window', function($scope, $window){
+    $('body').scrollspy({ target: '#affix-nav', offset:70})
+
+    $scope.scrollInto = function (elementId) {
+		console.log("scollInto:"+elementId)
+		if(!elementId) $window.scrollTo(0, 0);
+		//check if an element can be found with id attribute
+        
+        var el = document.getElementById(elementId);
+        if(el) {
+            console.log(el.offsetTop);
+            //$window.scrollTo(0,el.offsetTop-60);//scrollIntoView();
+            //$(el).parent().addClass("active");
+            //$(el).parent().removeClass("hover");
+            $($window).scrollTo($(el),1000,  {offset:-60} );//scrollIntoView();
+        }
+    }
+}])
+
+app.controller('competController', ['$scope', '$window', function($scope, $window){
+    $('body').scrollspy({ target: '#affix-nav', offset:70})
+
+    $scope.scrollInto = function (elementId) {
+		console.log("scollInto:"+elementId)
+		if(!elementId) $window.scrollTo(0, 0);
+		//check if an element can be found with id attribute
+        
+        var el = document.getElementById(elementId);
+        if(el) {
+            console.log(el.offsetTop);
+            //$window.scrollTo(0,el.offsetTop-60);//scrollIntoView();
+            //$(el).parent().addClass("active");
+            //$(el).parent().removeClass("hover");
+            $($window).scrollTo($(el),1000,  {offset:-60} );//scrollIntoView();
         }
     }
 }])
@@ -45,7 +85,7 @@ app.controller('clubController', ['$scope', '$window', function($scope, $window)
 app.controller('homeController', ['$scope' , '$window', function($scope, $window) {
     $(function(){
         $('.carousel').carousel({
-            interval: 2000
+            interval: 3000
         });
     });
 }])
