966f8a0da1632659f4757c9f10105363529c0a03
xavier blanc <blancxav@gmail.com>
2015-01-05 11:41:45 +0100

    test control formulaire

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,97 +1,115 @@
 'use strict';
 
-var ctrls=angular.module('controllersBMX',['servicesBMX'])//, 'waypoint-module'])
+var ctrls = angular.module('controllersBMX', ['servicesBMX']) //, 'waypoint-module'])
 
 
-ctrls.controller('clubController', ['$scope', '$window', function($scope, $window){
-    $('body').scrollspy({ target: '#affix-nav', offset:70})
+ctrls.controller('clubController', ['$scope', '$window', function($scope, $window) {
+    $('body').scrollspy({
+        target: '#affix-nav',
+        offset: 70
+    })
 
-    $scope.scrollInto = function (elementId) {
-		if(!elementId) $window.scrollTo(0, 0);
-		//check if an element can be found with id attribute
-        
+    $scope.scrollInto = function(elementId) {
+        if (!elementId) $window.scrollTo(0, 0);
+        //check if an element can be found with id attribute
+
         var el = document.getElementById(elementId);
-        if(el) {
+        if (el) {
             //$window.scrollTo(0,el.offsetTop-60);//scrollIntoView();
             //$(el).parent().addClass("active");
             //$(el).parent().removeClass("hover");
-            $($window).scrollTo($(el),1000,  {offset:-60} );//scrollIntoView();
+            $($window).scrollTo($(el), 1000, {
+                offset: -60
+            }); //scrollIntoView();
         }
     }
 }])
 
-ctrls.controller('animationController', ['$scope', '$window', function($scope, $window){
-    $('body').scrollspy({ target: '#affix-nav', offset:70})
+ctrls.controller('animationController', ['$scope', '$window', function($scope, $window) {
+    $('body').scrollspy({
+        target: '#affix-nav',
+        offset: 70
+    })
 
-    $scope.scrollInto = function (elementId) {
-		console.log("scollInto:"+elementId)
-		if(!elementId) $window.scrollTo(0, 0);
-		//check if an element can be found with id attribute
-        
+    $scope.scrollInto = function(elementId) {
+        console.log("scollInto:" + elementId)
+        if (!elementId) $window.scrollTo(0, 0);
+        //check if an element can be found with id attribute
+
         var el = document.getElementById(elementId);
-        if(el) {
+        if (el) {
             //$window.scrollTo(0,el.offsetTop-60);//scrollIntoView();
             //$(el).parent().addClass("active");
             //$(el).parent().removeClass("hover");
-            $($window).scrollTo($(el),1000,  {offset:-60} );//scrollIntoView();
+            $($window).scrollTo($(el), 1000, {
+                offset: -60
+            }); //scrollIntoView();
         }
     }
 }])
 
-ctrls.controller('competController', ['$scope', '$window', 'EventsService', 'InscriptionsService' , function($scope, $window, $EventsService, $InscriptionsService){
-    $('body').scrollspy({ target: '#affix-nav', offset:70})
+ctrls.controller('competController', ['$scope', '$window', 'EventsService', 'InscriptionsService', function($scope, $window, $EventsService, $InscriptionsService) {
+    $('body').scrollspy({
+        target: '#affix-nav',
+        offset: 70
+    })
 
-    $scope.inscriptions_ouvertes= $EventsService.query({'inscription':true})
+    $scope.inscriptions_ouvertes = $EventsService.query({
+        'inscription': true
+    })
 
-    $scope.new_inscription={}
+    $scope.new_inscription = {}
 
 
     //Calendar
-    $scope.calendar = $("#calendar").calendar(
-        {
-            tmpl_path: "components/bootstrap-calendar/tmpls/",
-            language: 'fr-FR',
-            events_source: './events'
+    $scope.calendar = $("#calendar").calendar({
+        tmpl_path: "components/bootstrap-calendar/tmpls/",
+        language: 'fr-FR',
+        events_source: './events'
             //events_source: './events-calendar'
-        }
-    ); 
+    });
 
     $scope.calendar.view('year');
 
     //Scrolling
-    $scope.scrollInto = function (elementId) {
-		console.log("scollInto:"+elementId)
-		if(!elementId) $window.scrollTo(0, 0);
-		//check if an element can be found with id attribute
-        
+    $scope.scrollInto = function(elementId) {
+        console.log("scollInto:" + elementId)
+        if (!elementId) $window.scrollTo(0, 0);
+        //check if an element can be found with id attribute
+
         var el = document.getElementById(elementId);
-        if(el) {
+        if (el) {
             console.log(el.offsetTop);
             //$window.scrollTo(0,el.offsetTop-60);//scrollIntoView();
             //$(el).parent().addClass("active");
             //$(el).parent().removeClass("hover");
-            $($window).scrollTo($(el),1000,  {offset:-60} );//scrollIntoView();
+            $($window).scrollTo($(el), 1000, {
+                offset: -60
+            }); //scrollIntoView();
         }
     }
 
     //modal inscription
-    $scope.showModal = function () {
-        $scope.new_inscription={}
-        $scope.new_inscription.success=false
-        $scope.new_inscription.already=false
+    $scope.showModal = function() {
+        $scope.new_inscription = {}
+        $scope.new_inscription.success = false
+        $scope.new_inscription.already = false
         $('#inscription-modal').modal('show')
     }
 
-    $scope.hideModal = function () {
+    $scope.hideModal = function() {
         $('#inscription-modal').modal('hide')
     }
 
     $scope.saveNewInscription = function() {
-        $InscriptionsService.save($scope.new_inscription , function(response) {
-            console.log("save inscription"+JSON.stringify(response))
-            if (response.result===1) $scope.new_inscription.success=true
-            else if (response.result===2) $scope.new_inscription.already=true
-        })
+        if (!$scope.form_inscription.$valid) console.log("form not valide")
+        else {
+            $InscriptionsService.save($scope.new_inscription, function(response) {
+                console.log("save inscription" + JSON.stringify(response))
+                if (response.result === 1) $scope.new_inscription.success = true
+                else if (response.result === 2) $scope.new_inscription.already = true
+            })
+        }
 
 
         //$scope.hideModal()
@@ -99,8 +117,8 @@ ctrls.controller('competController', ['$scope', '$window', 'EventsService', 'Ins
 }])
 
 
-ctrls.controller('homeController', ['$scope' , function($scope) {
-    $(function(){
+ctrls.controller('homeController', ['$scope', function($scope) {
+    $(function() {
         $('.carousel').carousel({
             interval: 3000
         });
@@ -108,47 +126,49 @@ ctrls.controller('homeController', ['$scope' , function($scope) {
 }])
 
 
-ctrls.controller('adminController', ['$scope' , 'EventsService', 'InscriptionsService' , function($scope , $EventsService, $InscriptionsService) {
-    $scope.events=[]
+ctrls.controller('adminController', ['$scope', 'EventsService', 'InscriptionsService', function($scope, $EventsService, $InscriptionsService) {
+    $scope.events = []
     $scope.events = $EventsService.query();
 
-    $scope.new_event= {}
+    $scope.new_event = {}
 
     //bootstrap pop
-    $(function () {
+    $(function() {
         $('[data-toggle="popover"]').popover()
     })
 
-    $scope.showModal = function () {
+    $scope.showModal = function() {
         $('#new-event-modal').modal('show')
     }
 
-    $scope.hideModal = function () {
+    $scope.hideModal = function() {
         $('#new-event-modal').modal('hide')
     }
 
     $scope.saveNewEvent = function() {
         $scope.new_event["start"] = $scope.new_event["start"].getTime()
         $scope.new_event["end"] = $scope.new_event["end"].getTime()
-        $scope.new_event.class= "event-important"
-        //$scope.new_event.id= $scope.events.length
+        $scope.new_event.class = "event-important"
+            //$scope.new_event.id= $scope.events.length
         $EventsService.save($scope.new_event)
         $scope.events.push($scope.new_event)
 
-        $scope.new_event={}
+        $scope.new_event = {}
 
         $scope.hideModal()
     }
 
-    $scope.remove = function (event) {
+    $scope.remove = function(event) {
         $EventsService.remove(event)
-        $scope.events= $EventsService.query()
+        $scope.events = $EventsService.query()
     }
 
-    $scope.show = function (event) {
-        $scope.show_event= event;
+    $scope.show = function(event) {
+        $scope.show_event = event;
 
-        $scope.show_event.inscriptions= $InscriptionsService.query({'event_id': event._id} , function(inscriptions) {
+        $scope.show_event.inscriptions = $InscriptionsService.query({
+            'event_id': event._id
+        }, function(inscriptions) {
             setCSVLink(inscriptions)
         })
 
@@ -157,9 +177,9 @@ ctrls.controller('adminController', ['$scope' , 'EventsService', 'InscriptionsSe
     }
 
     function inscription2CSV(inscriptions) {
-        var result="nom,prenom,licence\n"
+        var result = "nom,prenom,licence\n"
         for (var i = inscriptions.length - 1; i >= 0; i--) {
-            result+=inscriptions[i].nom+","+inscriptions[i].prenom+","+inscriptions[i].licence+"\n"
+            result += inscriptions[i].nom + "," + inscriptions[i].prenom + "," + inscriptions[i].licence + "\n"
         };
         console.log(result)
         return result
@@ -172,20 +192,22 @@ ctrls.controller('adminController', ['$scope' , 'EventsService', 'InscriptionsSe
 
         if (link.download !== undefined) { // feature detection
             // Browsers that support HTML5 download attribute
-            var blob = new Blob([inscription2CSV(inscriptions)], { type: 'text/csv;charset=utf-8;' });
-            var url = URL.createObjectURL(blob);            
+            var blob = new Blob([inscription2CSV(inscriptions)], {
+                type: 'text/csv;charset=utf-8;'
+            });
+            var url = URL.createObjectURL(blob);
             link.setAttribute("href", url);
             link.setAttribute("download", "inscriptions.csv");
             //link.style = "visibility:hidden";
         }
 
         if (navigator.msSaveBlob) { // IE 10+
-           link.addEventListener("click", function (event) {
-             var blob = new Blob([inscription2CSV(inscriptions)], {
-               "type": "text/csv;charset=utf-8;"
-             });
-           navigator.msSaveBlob(blob, "inscriptions.csv");
-          }, false);
+            link.addEventListener("click", function(event) {
+                var blob = new Blob([inscription2CSV(inscriptions)], {
+                    "type": "text/csv;charset=utf-8;"
+                });
+                navigator.msSaveBlob(blob, "inscriptions.csv");
+            }, false);
         }
 
         //document.body.appendChild(link);
